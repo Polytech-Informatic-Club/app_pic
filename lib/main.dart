b3744465bad9307d545201511676d0090a16e4a9
killer-pro <cissemouhamadoudiouf@gmail.com>
2024-09-22 20:40:23 +0000

    shop

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:new_app/UserPage.dart';
+//import 'package:new_app/UserPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:new_app/login/inscription.dart';
 import 'package:new_app/login/login.dart';
@@ -9,6 +9,7 @@ import 'package:new_app/pages/home/home_page.dart';
 import 'package:new_app/pages/sports/PagesSports/detail_match.dart';
 import 'package:new_app/pages/sports/PagesSports/foot.dart';
 import 'package:new_app/pages/sports/interclasse.dart';
+import 'package:new_app/pages/shop/article.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: Articles(),
     );
   }
 }
