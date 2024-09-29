8d6b96f4a493a95969eed0bd950945aee1f7cc17
codefun <codeforfun77@gmail.com>
2024-09-29 14:12:06 +0000

    New attributes added to ObjetPerdu model

--- lib/models/objet_perdu_model.dart
+++ lib/models/objet_perdu_model.dart
@@ -4,30 +4,33 @@ import 'package:new_app/models/enums/statut_objet_perdu.dart';
 
 class ObjetPerdu {
   final String description; // Description of the lost object
+  final String details;
   final String? photoURL;
   final String lieu; // Place where the object was lost
-  final DateTime date; // Date when the object was lost
+  final String date; // Date when the object was lost
   final int estTrouve; // Status of the object (PERDU or RETROUVE)
-  final Map<String, dynamic> user; // celui qui a signalé l'objet perdu
+  final String? idUser; // celui qui a signalé l'objet perdu
 
   ObjetPerdu({
     required this.description,
+    required this.details,
     required this.photoURL,
     required this.lieu,
     required this.date,
     required this.estTrouve,
-    required this.user
+    required this.idUser
   });
 
   // Factory method to create an ObjetPerdu object from JSON
   factory ObjetPerdu.fromJson(Map<String, dynamic> json) {
     return ObjetPerdu(
       description: json['description'] as String,
+      details: json['details'],
       photoURL: json['photoURL'] as String,
       lieu: json['lieu'] as String,
-      date: DateTime.parse(json['date'] as String),
+      date: json['date'],
       estTrouve: json['etat'] as int,
-      user: json['user'] as Map<String, dynamic>,
+      idUser: json['user'],
     );
   }
 
@@ -36,9 +39,9 @@ class ObjetPerdu {
       'description': description,
       'photoURL': photoURL,
       'lieu': lieu,
-      'date': date.toIso8601String(),
+      'date': date,
       'etat': estTrouve.toString(),
-      'user': user,
+      'user': idUser,
     };
   }
 }
